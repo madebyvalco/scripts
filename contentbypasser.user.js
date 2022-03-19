d0fb8352ab7e09fb8d74987da57c91e1f3a7a864
Rise Valco <80554644+RiseValco@users.noreply.github.com>
2022-03-19 15:57:10 -0400

    Create contentbypasser.user.js

---
+++ contentbypasser.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         mboost.me & rekonise.com Bypass
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Bypasser for mboost.me & rekonise.com
+// @author       Valco#7818
+// @match        https://rekonise.com/*
+// @match        https://mboost.me/a/*
+// @icon         https://www.google.com/s2/favicons?domain=mboost.me
+// @grant        none
+// ==/UserScript==
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+window.onload = function() {
+
+    if (document.location.origin == "https://rekonise.com") {
+
+        setTimeout(() => {
+            const button = document.getElementsByClassName("mat-focus-indicator unlock-button mat-flat-button mat-button-base mat-accent mat-button-disabled")[0]
+            document.getElementsByClassName("mat-focus-indicator unlock-button mat-flat-button mat-button-base mat-accent mat-button-disabled")[0].removeAttribute("disabled")
+            button.click()
+        }, 1000);
+
+    } else if (document.location.origin == "https://mboost.me") {
+
+        setTimeout(() => {
+            const jsonData = document.getElementById("__NEXT_DATA__").innerHTML
+            const formattedJson = JSON.parse(jsonData)
+
+            window.location.href = formattedJson.props.initialProps.pageProps.data.targeturl
+        }, 1000);
+    }
+}
