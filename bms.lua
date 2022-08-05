62fa7eef599e34530173371e69977ae6357fd41a
Rise Valco <80554644+RiseValco@users.noreply.github.com>
2022-08-04 21:14:44 -0400

    Update bms.lua

--- bms.lua
+++ bms.lua
@@ -19,7 +19,6 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 local plr = game:GetService("Players").LocalPlayer
 
 local Money = 10000000000
-getgenv().AutoFarm = false
 
 function BuyablesLeft()
     local Buyables = {}
