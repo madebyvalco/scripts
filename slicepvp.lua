f2157d61926275783a02003162430a16b780cdda
Rise Valco <80554644+RiseValco@users.noreply.github.com>
2022-09-09 23:15:11 -0400

    Update slicepvp.lua

--- slicepvp.lua
+++ slicepvp.lua
@@ -64,7 +64,7 @@ MainSection:NewToggle("Spam Balls", "Spams balls on everybody except you", funct
         for _,v in pairs(game:GetService("Players"):GetChildren()) do
         pcall(function()
             if plr ~= v then
-            local A_1 = "SummonBall"
+            local A_1 = "Summon43"
             local A_2 = v.Character.HumanoidRootPart.Position
             local Event = game:GetService("ReplicatedStorage").Events.RemoteMessenger.SendData
             Event:FireServer(A_1, A_2)   
@@ -86,7 +86,7 @@ MainSection:NewToggle("Spam Cylinders", "Spams Cylinders on everybody except you
         for _,v in pairs(game:GetService("Players"):GetChildren()) do
         pcall(function()
             if plr ~= v then
-            local A_1 = "SummonCylinder"
+            local A_1 = "Summon32"
             local A_2 = v.Character.HumanoidRootPart.Position
             local Event = game:GetService("ReplicatedStorage").Events.RemoteMessenger.SendData
             Event:FireServer(A_1, A_2)   
